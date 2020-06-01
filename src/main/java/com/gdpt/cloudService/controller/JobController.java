62953494620b05065615120e721dc750a749fc9f
yzhangm <yzhangm@qq.com>
2020-06-01 10:07:24 +0800

    使用方法
    进入pom.xml 文件里面,修改147行的Swagger文档地址
    
    mvn swagger2markup:convertSwagger2markup
    
    mvn generate-resources
    
    出现中文乱码问题，需要用asciidoctorj-pdf-1.5.0-alpha.10.1.jar代替默认的jar包，在这里面添加了几个中文字库，解决乱码问题

--- src/main/java/com/gdpt/cloudService/controller/JobController.java
+++
@@ -1,7 +0,0 @@
-package com.gdpt.cloudService.controller;
-
-/**
- * 作业对象控制器
- */
-public class JobController {
-}
--- src/main/java/com/gdpt/cloudService/controller/TaskController.java
+++
@@ -1,8 +0,0 @@
-package com.gdpt.cloudService.controller;
-
-/**
- * 任务控制器
- * 任务把机器人和作业对象关联起来
- */
-public class TaskController {
-}
